a2802d5cec4e18d13b6149c45430117878a9fa22
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-11-30 14:13:34 -0600

    feat: Dice roll exercise comopletion

--- src/main/java/com/codeup/blog/DiceRoll.java
+++ src/main/java/com/codeup/blog/DiceRoll.java
@@ -4,22 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Controller
- class DiceRoll {
-    @GetMapping("/roll-dice")
-    @ResponseBody
-    public String userInput(int x){
-       return "Rolled number is: 5";
-    }
-    @PostMapping("/roll-dice/{n}")
-   @ResponseBody
-   public String joinDice(@RequestParam(name = "aNumber") int n, Model model) {
-        model.addAttribute("aNumber", "You guessed " + n);
-        if (n != 5){
-            return ("You guessed incorrect!");
-        }else{
-            return ("You guessed correct");
-        }
+ class DiceRollController {
+    @GetMapping("/roll-dice/{n}")
+    public String rollDiceUser(@PathVariable int n, Model model) {
+        int randomNum = ThreadLocalRandom.current().nextInt(1,6+1);
+        model.addAttribute("randomNumber", randomNum);
+        model.addAttribute("userGuess", n);
+        model.addAttribute("isUserGuessCorrect", n == randomNum);
+        return "roll-dice";
     }
 
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
 }
--- src/main/resources/templates/partials/footer.jsp
+++ src/main/resources/templates/partials/footer.jsp
@@ -2,13 +2,13 @@
   Created by IntelliJ IDEA.
   User: roccopaccione
   Date: 11/30/20
-  Time: 12:00 AM
+  Time: 12:39 AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Navbar</title>
+    <title>Title</title>
 </head>
 <body>
 
