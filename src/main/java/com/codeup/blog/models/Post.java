0ab544b4443ffd0a5a9b112b20970447d935a370
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-12-01 12:27:52 -0600

    feat: created database for blog and posts tables

--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -1,28 +1,58 @@
 package com.codeup.blog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
     private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
 
-    public Post(){
-    }
+    @Column(nullable = false)
+    private String author;
 
-    public Post(String title, String body){
+    public Post() {}
+
+    public Post (long id, String title, String body, String author) {
+        this.id = id;
         this.title = title;
         this.body = body;
+        this.author = author;
     }
 
+    public Post(String s, String s1) {
+    }
+
+    public long getId() {
+        return id;
+    }
     public String getTitle() {
-        return title;       // Getter Title
+        return title;
     }
     public String getBody() {
-        return body;        // Getter Body
+        return body;
+    }
+    public String getAuthor() {
+        return author;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
     public void setTitle(String title) {
-        this.title = title;     // Setter Title
+        this.title = title;
     }
     public void setBody(String body) {
-        this.body = body;       // Setter Body
+        this.body = body;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
